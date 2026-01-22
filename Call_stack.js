58ba514a15b2557e113208422129239a21274be5
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2026-01-22 12:00:30 +0530

    22/1

---
+++ Call_stack.js
@@ -0,0 +1,11 @@
+/*
+1)global execution --- 
+functions 
+one()
+    -> two()
+            -> three()
+
+three() is within two() and two() is within one()
+
+// it follows LIFO- Last in first out
+*/
